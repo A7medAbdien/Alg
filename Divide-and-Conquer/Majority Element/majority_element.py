72019bcb54fd76fae72992d4ea728093a43bc554
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-10-03 13:48:29 +0300

    solve the majority sort

--- Divide-and-Conquer/Majority Element/majority_element.py
+++ Divide-and-Conquer/Majority Element/majority_element.py
@@ -12,19 +12,48 @@ def majority_element_naive(elements):
 
 def majority_element(elements):
     assert len(elements) <= 10 ** 5
-    for e in elements:
-        # print(f"{e} {elements.count(e)} {len(elements) // 2}")
-        if (elements.count(e) > len(elements) // 2) & (len(elements) // 2 != 0):
+    # print(elements)
+
+    m = len(elements)//2
+    a = most(elements[m:])
+    b = most(elements[:m])
+    # print(f"a: {a}")
+    # print(f"b: {b}")
+    if list == type(a):
+        if b in a:
             return 1
-            break
-        return 0 + majority_element(elements[len(elements) // 2:0]) + majority_element(elements[:len(elements) // 2])
+    if list == type(b):
+        if a in b:
+            return 1
+    if (list != type(a)) or (list != type(b)):
+        if a == b:
+            return 1
     return 0
 
 
+def most(elements):
+    # print(elements)
+    for e in elements:
+        if elements.count(e) > len(elements) / 2:
+            return e
+    return elements
+
+
+# find most appeared
+def most_e(elements):
+    m = len(elements) // 2
+    if len(elements) > 2:
+        return majority_element(elements[m:]) + majority_element(elements[:m])
+    for e in elements:
+        print(f"{e} {elements.count(e)} {len(elements) // 2}")
+        if (elements.count(e) > m) & (m != 0):
+            return 1
+
+
 if __name__ == '__main__':
-    input_n = int(input())
-    input_elements = list(map(int, input().split()))
-    # input_elements = [1, 2, 3, 4]
-    assert len(input_elements) == input_n
+    # input_n = int(input())
+    # input_elements = list(map(int, input().split()))
+    input_elements = [1, 1,2]
+    # assert len(input_elements) == input_n
     print(majority_element(input_elements))
-    # print(majority_element_naive(input_elements))
+    print(majority_element_naive(input_elements))
